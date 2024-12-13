880b5193eb81a033a22ed0615b18b9342e7eaf07
Nicolas Ga <nicogaston8@gmail.com>
2024-12-13 20:52:32 -0300

    Primer commit del trabajo final

---
+++ TpFinalPlataformaGrupo6/Controllers/PlazoFijoesController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TpFinalPlataformaGrupo6.Date;
+using TpFinalPlataformaGrupo6.Models;
+
+namespace TpFinalPlataformaGrupo6.Controllers
+{
+    public class PlazoFijoesController : Controller
+    {
+        private readonly MyContext _context;
+
+        public PlazoFijoesController(MyContext context)
+        {
+            _context = context;
+        }
+
+        // GET: PlazoFijoes
+        public async Task<IActionResult> Index()
+        {
+            var myContext = _context.plazoFijo.Include(p => p.titular);
+            return View(await myContext.ToListAsync());
+        }
+
+        // GET: PlazoFijoes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.plazoFijo == null)
+            {
+                return NotFound();
+            }
+
+            var plazoFijo = await _context.plazoFijo
+                .Include(p => p.titular)
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (plazoFijo == null)
+            {
+                return NotFound();
+            }
+
+            return View(plazoFijo);
+        }
+
+        // GET: PlazoFijoes/Create
+        public IActionResult Create()
+        {
+            ViewData["num_usr"] = new SelectList(_context.usuarios, "id_usuario", "apellido");
+            return View();
+        }
+
+        // POST: PlazoFijoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id,num_usr,monto,fechaIni,fechaFin,tasa,pagado")] PlazoFijo plazoFijo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(plazoFijo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["num_usr"] = new SelectList(_context.usuarios, "id_usuario", "apellido", plazoFijo.num_usr);
+            return View(plazoFijo);
+        }
+
+        // GET: PlazoFijoes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.plazoFijo == null)
+            {
+                return NotFound();
+            }
+
+            var plazoFijo = await _context.plazoFijo.FindAsync(id);
+            if (plazoFijo == null)
+            {
+                return NotFound();
+            }
+            ViewData["num_usr"] = new SelectList(_context.usuarios, "id_usuario", "apellido", plazoFijo.num_usr);
+            return View(plazoFijo);
+        }
+
+        // POST: PlazoFijoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("id,num_usr,monto,fechaIni,fechaFin,tasa,pagado")] PlazoFijo plazoFijo)
+        {
+            if (id != plazoFijo.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(plazoFijo);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PlazoFijoExists(plazoFijo.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["num_usr"] = new SelectList(_context.usuarios, "id_usuario", "apellido", plazoFijo.num_usr);
+            return View(plazoFijo);
+        }
+
+        // GET: PlazoFijoes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.plazoFijo == null)
+            {
+                return NotFound();
+            }
+
+            var plazoFijo = await _context.plazoFijo
+                .Include(p => p.titular)
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (plazoFijo == null)
+            {
+                return NotFound();
+            }
+
+            return View(plazoFijo);
+        }
+
+        // POST: PlazoFijoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.plazoFijo == null)
+            {
+                return Problem("Entity set 'MyContext.plazoFijo'  is null.");
+            }
+            var plazoFijo = await _context.plazoFijo.FindAsync(id);
+            if (plazoFijo != null)
+            {
+                _context.plazoFijo.Remove(plazoFijo);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PlazoFijoExists(int id)
+        {
+            return (_context.plazoFijo?.Any(e => e.id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ TpFinalPlataformaGrupo6/Controllers/UsuarioCajaDeAhorroesController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TpFinalPlataformaGrupo6.Date;
+using TpFinalPlataformaGrupo6.Models;
+
+namespace TpFinalPlataformaGrupo6.Controllers
+{
+    public class UsuarioCajaDeAhorroesController : Controller
+    {
+        private readonly MyContext _context;
+
+        public UsuarioCajaDeAhorroesController(MyContext context)
+        {
+            _context = context;
+        }
+
+        // GET: UsuarioCajaDeAhorroes
+        public async Task<IActionResult> Index()
+        {
+            var myContext = _context.UsuarioCajaDeAhorro.Include(u => u.cajaAhorro).Include(u => u.usuario);
+            return View(await myContext.ToListAsync());
+        }
+
+        // GET: UsuarioCajaDeAhorroes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.UsuarioCajaDeAhorro == null)
+            {
+                return NotFound();
+            }
+
+            var usuarioCajaDeAhorro = await _context.UsuarioCajaDeAhorro
+                .Include(u => u.cajaAhorro)
+                .Include(u => u.usuario)
+                .FirstOrDefaultAsync(m => m.id_UsuarioCaja == id);
+            if (usuarioCajaDeAhorro == null)
+            {
+                return NotFound();
+            }
+
+            return View(usuarioCajaDeAhorro);
+        }
+
+        // GET: UsuarioCajaDeAhorroes/Create
+        public IActionResult Create()
+        {
+            ViewData["fk_cajaAhorro"] = new SelectList(_context.cajaAhorro, "id", "id");
+            ViewData["fk_usuario"] = new SelectList(_context.usuarios, "id_usuario", "apellido");
+            return View();
+        }
+
+        // POST: UsuarioCajaDeAhorroes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id_UsuarioCaja,fk_usuario,fk_cajaAhorro")] UsuarioCajaDeAhorro usuarioCajaDeAhorro)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(usuarioCajaDeAhorro);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["fk_cajaAhorro"] = new SelectList(_context.cajaAhorro, "id", "id", usuarioCajaDeAhorro.fk_cajaAhorro);
+            ViewData["fk_usuario"] = new SelectList(_context.usuarios, "id_usuario", "apellido", usuarioCajaDeAhorro.fk_usuario);
+            return View(usuarioCajaDeAhorro);
+        }
+
+        // GET: UsuarioCajaDeAhorroes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.UsuarioCajaDeAhorro == null)
+            {
+                return NotFound();
+            }
+
+            var usuarioCajaDeAhorro = await _context.UsuarioCajaDeAhorro.FindAsync(id);
+            if (usuarioCajaDeAhorro == null)
+            {
+                return NotFound();
+            }
+            ViewData["fk_cajaAhorro"] = new SelectList(_context.cajaAhorro, "id", "id", usuarioCajaDeAhorro.fk_cajaAhorro);
+            ViewData["fk_usuario"] = new SelectList(_context.usuarios, "id_usuario", "apellido", usuarioCajaDeAhorro.fk_usuario);
+            return View(usuarioCajaDeAhorro);
+        }
+
+        // POST: UsuarioCajaDeAhorroes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("id_UsuarioCaja,fk_usuario,fk_cajaAhorro")] UsuarioCajaDeAhorro usuarioCajaDeAhorro)
+        {
+            if (id != usuarioCajaDeAhorro.id_UsuarioCaja)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(usuarioCajaDeAhorro);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UsuarioCajaDeAhorroExists(usuarioCajaDeAhorro.id_UsuarioCaja))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["fk_cajaAhorro"] = new SelectList(_context.cajaAhorro, "id", "id", usuarioCajaDeAhorro.fk_cajaAhorro);
+            ViewData["fk_usuario"] = new SelectList(_context.usuarios, "id_usuario", "apellido", usuarioCajaDeAhorro.fk_usuario);
+            return View(usuarioCajaDeAhorro);
+        }
+
+        // GET: UsuarioCajaDeAhorroes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.UsuarioCajaDeAhorro == null)
+            {
+                return NotFound();
+            }
+
+            var usuarioCajaDeAhorro = await _context.UsuarioCajaDeAhorro
+                .Include(u => u.cajaAhorro)
+                .Include(u => u.usuario)
+                .FirstOrDefaultAsync(m => m.id_UsuarioCaja == id);
+            if (usuarioCajaDeAhorro == null)
+            {
+                return NotFound();
+            }
+
+            return View(usuarioCajaDeAhorro);
+        }
+
+        // POST: UsuarioCajaDeAhorroes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.UsuarioCajaDeAhorro == null)
+            {
+                return Problem("Entity set 'MyContext.UsuarioCajaDeAhorro'  is null.");
+            }
+            var usuarioCajaDeAhorro = await _context.UsuarioCajaDeAhorro.FindAsync(id);
+            if (usuarioCajaDeAhorro != null)
+            {
+                _context.UsuarioCajaDeAhorro.Remove(usuarioCajaDeAhorro);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UsuarioCajaDeAhorroExists(int id)
+        {
+            return (_context.UsuarioCajaDeAhorro?.Any(e => e.id_UsuarioCaja == id)).GetValueOrDefault();
+        }
+    }
+}
